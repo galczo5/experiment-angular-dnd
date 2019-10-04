4dd25ab97b8e3674f86a23c468609e15a8ef0da8
galczo5 <kamil.galek@gmail.com>
2019-10-04 22:23:08 +0200

    Added styling to drag and drop elements

--- projects/angular-dnd/src/lib/directives/drop.directive.ts
+++ projects/angular-dnd/src/lib/directives/drop.directive.ts
@@ -4,6 +4,8 @@ import {DndStoreService} from '../services/dnd-store.service';
 import {DndEvent} from '../types/DndEvents';
 import {fromEvent, Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
+import {DndStylesService} from '../services/dnd-styles.service';
+import {DndCss} from '../types/DndCss';
 
 @Directive({
   selector: '[dndDrop]'
@@ -14,12 +16,13 @@ export class DropDirective implements OnInit {
   dropped: EventEmitter<any> = new EventEmitter<any>();
 
   private drag$: Subject<void> = new Subject<void>();
-  private nativeElement: HTMLElement;
+  private readonly nativeElement: HTMLElement;
 
   constructor(private readonly renderer: Renderer2,
               private readonly zone: NgZone,
               private readonly eventsService: DndEventsService,
               private readonly storeService: DndStoreService,
+              private readonly stylesService: DndStylesService,
               el: ElementRef) {
     this.nativeElement = el.nativeElement;
   }
@@ -30,21 +33,37 @@ export class DropDirective implements OnInit {
         if (event === DndEvent.DRAG_STARTED) {
           this.dragStart();
         } else if (event === DndEvent.DRAG_ENDED) {
-          this.drag$.next();
+          this.dragEnd();
         }
       });
   }
 
+  private dragEnd(): void {
+    this.stylesService.removeClass(this.nativeElement, DndCss.DROP);
+    this.stylesService.removeClass(this.nativeElement, DndCss.DROP_ACTIVE);
+    this.drag$.next();
+  }
+
   private dragStart(): void {
+    this.stylesService.addClass(this.nativeElement, DndCss.DROP);
+    this.registerDragListeners();
+  }
+
+  private registerDragListeners() {
     fromEvent(this.nativeElement, 'mouseup')
       .pipe(takeUntil(this.drag$))
       .subscribe((event: MouseEvent) => {
         event.stopPropagation();
-
-        const dragData = this.storeService.get();
         this.eventsService.itemsDropped();
-        this.dropped.emit(dragData.payload);
+        this.dropped.emit(this.storeService.get().payload);
       });
-  }
 
+    fromEvent(this.nativeElement, 'mouseenter')
+      .pipe(takeUntil(this.drag$))
+      .subscribe(() => this.stylesService.addClass(this.nativeElement, DndCss.DROP_ACTIVE));
+
+    fromEvent(this.nativeElement, 'mouseleave')
+      .pipe(takeUntil(this.drag$))
+      .subscribe(() => this.stylesService.removeClass(this.nativeElement, DndCss.DROP_ACTIVE));
+  }
 }
